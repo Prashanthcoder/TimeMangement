abbcf1363d289a7a0cd50c51c7cbfcf842961297
Prashanthcoder <tp120113@gmail.com>
2026-03-20 20:04:26 +0530

    just completed my own console app for time management subject wise

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 package timemanagement;
-import java.util.*;
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		Tracker track = new Tracker();
+		do {
 		System.out.println("Choose your option: ");
 		System.out.println("1. Add subject and Goal Hours ");
 		System.out.println("2. Log Hours worked");
@@ -15,10 +16,33 @@ public class Main {
 		System.out.println("6. Exit");
 		System.out.println("Enter your option: ");
 		int choice = sc.nextInt();
+		sc.nextLine();
 		switch(choice) {
-		case 1: 
+		case 1: System.out.println("Enter you subject name: ");
+		String name = sc.nextLine();
+		System.out.println("Enter your goalhours");
+		int hours = sc.nextInt();
+		track.addSubject(name, hours);
+		break;
+		case 2:System.out.println("enter the subject you worked for");
+		String sub = sc.nextLine();
+			System.out.println("Enter no. of hours worked");
+			int w_hours = sc.nextInt();
+		track.logHours(sub, w_hours);
+		break;
+		case 3: track.viewprogress();
+		break;
+		case 4: track.findTopSubject();
+		break;
+		case 5: track.checkGoals();
+		break;
+		case 6: System.out.println("Exiting...");
+		break;
+		default: System.out.println("Invalid choice choose in between 1-6");
+		break;
 		}
 			
+	}while(true);
 	}
 
 }
--- Tracker.java
+++ Tracker.java
@@ -21,8 +21,58 @@ public class Tracker {
 		void logHours(String name, int hours) {
 			for(int i = 0;i<count;i++) {
 				if(subjects[i].subject.equalsIgnoreCase(name)) {
+					if(hours<0) {
+						System.out.println("Invalid hours");
+						return;
+					}
 					
+					subjects[i].totalHours += hours;
+					System.out.println("Updated the hours!");
+					return;
 				}
 			}
+			System.out.println("Subject Not found");
 		}
+		
+		void viewprogress() {
+			if(count ==0) {
+				System.out.println("No subject added");
+				return;
+			}
+			for(int i =0;i<count;i++) {
+				Subject s = subjects[i];
+				double percent = (s.totalHours*100)/s.goalHours;
+				System.out.println(s.subject+" -> " + s.totalHours+"/"+s.goalHours+" hours ("+percent+"%");
+			}
+		}
+
+
+void findTopSubject() {
+	if(count ==0) {
+		System.out.println("There is no subject found");
+		return;
+	}
+	Subject top = subjects[0];
+	for(int i = 1;i<count;i++) {
+		if(subjects[i].totalHours> top.totalHours) {
+			top = subjects[i];
+		}
+		
+	}
+	System.out.println("Top subject : "+top.subject);
+
+
+}
+void checkGoals() {
+	boolean found = false;
+	for(int i =0;i<count;i++) {
+		if(subjects[i].totalHours>=subjects[i].goalHours) {
+			System.out.println("Goal completed! "+subjects[i].subject);
+			found = true;
+		}
+	}
+	if(!found) {
+		System.out.println("No goals Completed yet!");
+	}
+}
 }
